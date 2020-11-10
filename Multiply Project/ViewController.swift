b04e231e7d3d6efc4933cf2f2500d9755427e476
Chris Mammen <cmammen2246@stu.d214.org>
2020-11-10 12:25:13 -0800

    Initial Commit

---
+++ Multiply Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Multiply Project
+//
+//  Created by user171406 on 11/10/20.
+//  Copyright © 2020 John Hersey Highschool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
